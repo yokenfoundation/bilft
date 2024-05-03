c453c4fdba22531280d32de002ceb0381bf08aad
XantreDev <valerik007.vs@gmail.com>
2024-05-03 22:56:49 +0200

    added user header

--- source/application.tsx
+++ source/application.tsx
@@ -11,10 +11,20 @@ const Application: React.FunctionComponent = () => {
   }, []);
 
   return (
-    <div className="application">
+    <main className="application">
+      <section className="px-6 py-4 flex flex-row gap-5 items-center">
+        <img
+          src="https://img.freepik.com/free-photo/vertical-closeup-shot-grey-cat-staring-camera-with-its-green-eyes_181624-45908.jpg?size=626&ext=jpg&ga=GA1.1.553209589.1714435200&semt=ais"
+          className="w-12 aspect-square rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <p className="font-bold text-[20px] leading-6">Jane</p>
+          <p className="text-[15px] leading-[22px]">Member since Jan 2021</p>
+        </div>
+      </section>
       <LoadingIndicator context={context} hidden={!context.loading.active()} />
       <Profile context={context}></Profile>
-    </div>
+    </main>
   );
 };
 
