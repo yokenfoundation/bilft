0adc8a0f6d19c7ca08e6455dfa6f00053719c3a6
XantreDev <valerik007.vs@gmail.com>
2024-05-09 02:18:34 +0200

    implemented back to top function

--- source/application.tsx
+++ source/application.tsx
@@ -24,6 +24,25 @@ const UserStatus = (props: PropsWithChildren<StyleProps>) => (
   </article>
 );
 
+const LoadingSvg = (props: StyleProps) => (
+  <svg
+    aria-hidden="true"
+    className={clsxString("inline aspect-square animate-spin", props.className ?? "")}
+    viewBox="0 0 100 101"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+      fill="currentColor"
+    />
+    <path
+      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+      fill="currentFill"
+    />
+  </svg>
+);
+
 function PostInput(props: { value: string; onChange: (s: string) => void; onSubmit: () => void; isLoading: boolean }) {
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const isEmpty = props.value.length === 0;
@@ -63,22 +82,7 @@ function PostInput(props: { value: string; onChange: (s: string) => void; onSubm
       >
         {props.isLoading ? (
           <div role="status">
-            <svg
-              aria-hidden="true"
-              className="inline w-7 h-7 animate-spin text-gray-600 fill-gray-300"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
+            <LoadingSvg className="text-gray-600 w-7 fill-gray-300" />
             <span className="sr-only">Loading...</span>
           </div>
         ) : (
@@ -235,21 +239,53 @@ const Application: React.FunctionComponent = () => {
         />
       </section>
 
-      {notesQuery.data && notesQuery.data?.length > 0 ? (
-        notesQuery.data.map((note) => (
-          <BoardNote key={note.id} createdAt={note.createdAt} avatarUrl={note.author.photo} name={note.author.name}>
-            {note.content}
-          </BoardNote>
-        ))
-      ) : (
-        <div className="p-8 flex flex-col items-center">
-          <img src="/assets/empty-notes.webp" className="w-32 aspect-square" alt="Questioning banana" />
-          <strong className="font-inter text-center font-medium text-[20px] leading-[25px] mt-6">
-            It's still empty
-          </strong>
-          <p className="text-[#AAA] font-inter text-center text-[17px] leading-[22px]">Be the first to post here!</p>
-        </div>
-      )}
+      <section className="pb-6 flex flex-col">
+        {notesQuery.data && notesQuery.data?.length > 0 ? (
+          <>
+            {notesQuery.data.map((note) => (
+              <BoardNote key={note.id} createdAt={note.createdAt} avatarUrl={note.author.photo} name={note.author.name}>
+                {note.content}
+              </BoardNote>
+            ))}
+
+            {notesQuery.isFetchingNextPage ? (
+              <div role="status" className="mx-auto mt-6">
+                <LoadingSvg className="fill-[#FF375F] w-8 text-transparent" />
+                <span className="sr-only">Next boards is loading</span>
+              </div>
+            ) : notesQuery.data.length >= 8 ? (
+              <button
+                onClick={() => {
+                  window.scrollTo({
+                    behavior: "smooth",
+                    top: 0,
+                  });
+                }}
+                className="font-inter flex items-center gap-x-2 mt-6 text-[17px] active:opacity-70 transition-opacity leading-[22px] mx-auto text-[#FF375F]"
+              >
+                Back to top
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M3 10L8 5L13 10"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            ) : null}
+          </>
+        ) : (
+          <div className="p-8 flex flex-col items-center">
+            <img src="/assets/empty-notes.webp" className="w-32 aspect-square" alt="Questioning banana" />
+            <strong className="font-inter text-center font-medium text-[20px] leading-[25px] mt-6">
+              It's still empty
+            </strong>
+            <p className="text-[#AAA] font-inter text-center text-[17px] leading-[22px]">Be the first to post here!</p>
+          </div>
+        )}
+      </section>
     </main>
   );
 };
