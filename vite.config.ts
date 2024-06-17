0f67b35fbf842f80aee04e25fb830faa7de74c98
XantreDev <valerik007.vs@gmail.com>
2024-06-17 11:11:51 +0200

    remove build before start

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,23 @@ import { defineConfig, loadEnv } from "vite";
 
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { rm } from "node:fs/promises";
 
 import solid from "vite-plugin-solid";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(async ({ mode, command }) => {
   const env = loadEnv(mode, ".");
   const backendUrl = env.VITE_BACKEND_URL;
 
   console.assert(backendUrl.length > 0, "backend url must be in env");
   console.log("app looking to domain", backendUrl);
+
+  if (command === "build") {
+    await rm("./docs", {
+      force: true,
+      recursive: true,
+    });
+  }
 
   return {
     server: {
