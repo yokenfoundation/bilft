ac6302fd38f46dbe4e511eef8b9758232ad34b05
XantreDev <valerik007.vs@gmail.com>
2024-06-26 11:01:04 +0200

    added solid eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import solid from "eslint-plugin-solid";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  solid.configs["flat/typescript"],
 );
